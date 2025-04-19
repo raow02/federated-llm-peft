65f451bbe61c12a26daf001d383f0238c3eb9542
wrao <raowang02@gmail.com>
2025-04-19 03:48:23 -0400

    update metric to llm evaluation

--- script/evaluate.sh
+++ script/evaluate.sh
@@ -2,18 +2,9 @@
 # Evaluation script for federated learning models
 
 # Run evaluation
-python metric.py \
-    --exp_name 'homo-1B' \
-    --target_file './data/dataset1/flan_test_200_selected_nstrict_1.jsonl' \
-    --target_key 'output' \
-    --prediction_dir './predictions' \
-    --prediction_key 'answer' \
-    --evaluation_dir './evaluations_new_metric' \
-    --communication_rounds 20
-
 python evaluate.py \
   --api_key="YOUR_OPENAI_KEY" \
-  --exp_name="fedavg-1B" \
+  --exp_name="homo-1B" \
   --prediction_dir="./predictions" \
   --baseline_file="baseline_output.jsonl" \
   --evaluation_dir './evaluations' \
