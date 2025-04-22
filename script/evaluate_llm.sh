c51944b53f0ec01d7d580c5306534f2419729c26
wrao <raowang02@gmail.com>
2025-04-22 05:22:50 -0400

    update for batch evaluation

--- script/evaluate_llm.sh
+++ script/evaluate_llm.sh
@@ -3,13 +3,14 @@
 
 # ------------------- Configurable Variables ------------------- #
 api_key=""
-gpt_model="o4-mini-2025-04-16"
+gpt_model="gpt-4.1-mini"
 is_global_model=false         # true or false
 exp_name="none-1B"
 communication_rounds=1
 proposed_file="homo-1B/20/global_output.jsonl"
 prediction_dir="./predictions"
 evaluation_dir="./evaluations_llm"
+batch_size=20
 
 
 # ------------------- Evaluation Logic ------------------- #
@@ -22,7 +23,8 @@ if [ "$is_global_model" = true ]; then
     --prediction_dir="$prediction_dir" \
     --proposed_file="$proposed_file" \
     --evaluation_dir="$evaluation_dir" \
-    --communication_rounds="$communication_rounds"
+    --communication_rounds="$communication_rounds" \
+    --batch_size="$batch_size"
 else
   echo "Evaluating client models..."
   for client_id in {0..7}; do
@@ -35,6 +37,7 @@ else
       --proposed_file="$proposed_file" \
       --evaluation_dir="$evaluation_dir" \
       --communication_rounds="$communication_rounds" \
-      --client_id="$client_id"
+      --batch_size="$batch_size" \
+      --client_id="$client_id" 
   done
 fi
